9ce08b8d1632731064285146e4eb873615c1cf3a
vovam <vovam@DESKTOP-F5IFSAN>
2023-04-14 13:47:04 +0300

    Добавьте файлы проекта.

---
+++ DataBase.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace WindowsFormsApp1
+{
+    internal class DataBase
+    {
+        SqlConnection sqlConnection = new SqlConnection(@"Data Source = DESKTOP-F5IFSAN\SQLEXPRESS; Initial Catalog = Documents_demo; Integrated Security = True");
+
+        public void OpenConnection()
+        {
+            if (sqlConnection.State == System.Data.ConnectionState.Closed)
+            {
+                sqlConnection.Open();
+            }
+        }
+
+        public void CloseConnection()
+        {
+            if (sqlConnection.State == System.Data.ConnectionState.Open)
+            {
+                sqlConnection.Close();
+            }
+        }
+
+        public SqlConnection GetConnection()
+        {
+            return sqlConnection;
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        DataBase DataBase = new DataBase();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private int count;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var login = textBox1.Text;
+            var pass = textBox2.Text;
+
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            DataTable table = new DataTable();
+
+            String loginquery = $"select login, password, isAdmin from users where login = '{login}' and password = '{pass}'";
+
+            SqlCommand command = new SqlCommand(loginquery, DataBase.GetConnection());
+
+            adapter.SelectCommand = command;
+            adapter.Fill(table);
+
+            if (table.Rows.Count == 1)
+            {
+                var user = new checkUser(table.Rows[0].ItemArray[0].ToString(), Convert.ToBoolean(table.Rows[0].ItemArray[2]));
+
+                MessageBox.Show("Вход выполнен успешно");
+                osnova osn = new osnova();
+                this.Hide();
+                osn.ShowDialog();
+                textBox1.Text = "";
+                textBox2.Text = "";
+            }
+            else
+            {
+                MessageBox.Show("Неверный логин или пароль");
+
+                if (count++ == 2)
+                {
+                    New_pas np = new New_pas();
+                    this.Hide();
+                    np.ShowDialog();
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
